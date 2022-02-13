5c9f778fe963960693c12610e05cddab016422b2
bg117 <kiangabrielarambulo@gmail.com>
2022-02-13 17:13:05 +0800

    switch to FAT16

--- bootloader/kernel-loader/src/main.c
+++ bootloader/kernel-loader/src/main.c
@@ -1,11 +1,12 @@
 #define _BAREBONES_C_ 1
 #define _BAREMETAL_ 1
+#include <stddef.h>
+#include <stdint.h>
 
-#include <stdio.h>
+_extern int print_char(char c);
 
-_extern int bootmain(void)
+_extern int bootmain(uint8_t drivenum)
 {
-    puts("Hello world!");
-
+    print_char('S');    
     return 0;
 }
